042483a4564cab8127306476dcca54eff119c030
KasiaKos81 <kasiakos81@gmail.com>
2022-05-07 10:29:16 +0200

    Commit scanner

--- src/main/java/pl/sda/arp4/console/Main.java
+++ src/main/java/pl/sda/arp4/console/Main.java
@@ -1,4 +1,21 @@
 package pl.sda.arp4.console;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // wyjście programu
+        System.out.println("Rozpoczynam działanie");
+
+        // wejście programu
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imię");
+
+        // wczytuje imie od użytkownika (jakikolwiek tekst - 1 słowo/token)
+        String zmiennaImie = scanner.next();
+        System.out.println("Witaj " + zmiennaImie + "!");
+    }
+
 }
